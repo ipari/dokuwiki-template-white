81b6c7d1204cf619c57af3c670aa9ca384a0b3db
Lee, Kwangyoung <ipari@leaflette.com>
2016-02-12 00:52:06 +0900

    add .ini localize text

---
+++ lang/en/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+// style.ini values
+
+$lang['__link__']     = 'The general link color';
+$lang['__existing__'] = 'The color for links to existing pages';
+$lang['__missing__']  = 'The color for links to non-existing pages';
+$lang['__site_width__']    = 'The width of the full site (%, px, em, ...)';
+$lang['__sidebar_width__'] = 'The width of the sidebar, if any (%, px, em, ...)';
---
+++ lang/ko/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['__link__']              = '일반 링크 색';
+$lang['__existing__']          = '문서가 존재하는 링크의 색';
+$lang['__missing__']           = '문서가 존재하지 않는 링크의 색';
+$lang['__site_width__']        = '전체 사이트의 너비 (%, px, em, ...)';
+$lang['__sidebar_width__']     = '사이드바가 있다면, 그것의 너비 (%, px, em, ...)';
