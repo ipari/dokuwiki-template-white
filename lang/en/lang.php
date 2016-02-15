9c5042ab97f13abe3602f474ed809f26fc9287bc
Lee, Kwangyoung <ipari@leaflette.com>
2016-02-15 23:07:31 +0900

    take out global font size and line height to style.ini

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,5 +5,7 @@
 $lang['__link__']     = 'The general link color';
 $lang['__existing__'] = 'The color for links to existing pages';
 $lang['__missing__']  = 'The color for links to non-existing pages';
+$lang['__font_size__'] = 'The global font size (%, px, pt)';
+$lang['__line_height__'] = 'The line height of text';
 $lang['__site_width__']    = 'The width of the full site (%, px, em, ...)';
 $lang['__sidebar_width__'] = 'The width of the sidebar, if any (%, px, em, ...)';
--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -8,5 +8,7 @@
 $lang['__link__']              = '일반 링크 색';
 $lang['__existing__']          = '문서가 존재하는 링크의 색';
 $lang['__missing__']           = '문서가 존재하지 않는 링크의 색';
-$lang['__site_width__']        = '전체 사이트의 너비 (%, px, em, ...)';
+$lang['__font_size__']         = '전체 사이트 글자 크기 (%, px, pt)';
+$lang['__line_height__']       = '전체 사이트 줄 간격';
+$lang['__site_width__']        = '전체 사이트 너비 (%, px, em, ...)';
 $lang['__sidebar_width__']     = '사이드바가 있다면, 그것의 너비 (%, px, em, ...)';
