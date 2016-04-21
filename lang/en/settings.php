e1c920ec3254aa0734ef90dd99ba5b1b61f48d35
Myeongjin <aranet100@gmail.com>
2016-04-21 21:36:09 +0900

    Change case for 'DokuWiki'

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -2,5 +2,5 @@
 
 $lang['numberedHeading']         = 'Numbering headings';
 $lang['tocPosition']             = 'Position of TOC';
-$lang['tocPosition_o_dokuwiki']  = 'Dokuwiki';
+$lang['tocPosition_o_dokuwiki']  = 'DokuWiki';
 $lang['tocPosition_o_wikipedia'] = 'Wikipedia';
