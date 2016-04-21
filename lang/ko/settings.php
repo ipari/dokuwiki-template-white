7951a3e2cb302be02ae98511d67070ab67eedb99
Myeongjin <aranet100@gmail.com>
2016-04-21 21:23:18 +0900

    Change Korean name for 'Wikipedia'

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -3,4 +3,4 @@
 $lang['numberedHeading']         = '제목 수준에 번호를 붙힙니다.';
 $lang['tocPosition']             = '목차의 위치';
 $lang['tocPosition_o_dokuwiki']  = '도쿠위키 기본';
-$lang['tocPosition_o_wikipedia'] = '위키피디아 스타일';
+$lang['tocPosition_o_wikipedia'] = '위키백과 스타일';
