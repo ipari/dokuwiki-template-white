c0f95b35b8609191d8cfc77e342063e53b3c0d62
ipari <ipari@yaong.ee>
2018-11-03 16:32:47 +0900

    Added French translation #14 from 7163D

---
+++ lang/fr/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 7163D from GitHub
+ */
+$lang['__link__']             = 'La couleur générale des liens';
+$lang['__existing__']         = 'La couleur des liens vers une page existante';
+$lang['__missing__']          = 'La couleur des liens vers une page qui n\'existe pas';
+$lang['__numbered_heading__'] = 'La couleur pour les numéros d\'en-tête';
+$lang['__font_size__']        = 'La taille globale de police (%, px, pt)';
+$lang['__line_height__']      = 'L\'épaisseur globale des lines (x.x, %)';
+$lang['__body_width__']       = 'La largeur du corps(%, px, em, ...)';
+$lang['__sidebar_width__']    = 'La taille de la barre latérale, si elle existe(%, px, em, ...)';
---
+++ lang/fr/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['numberedHeading']         = 'Nombre de titres';
+$lang['tocPosition']             = 'Position de la table des matières';
+$lang['tocPosition_o_dokuwiki']  = 'DokuWiki';
+$lang['tocPosition_o_wikipedia'] = 'Wikipedia';
+$lang['footer']                  = 'Pied de page, laisser vide pour désactiver le pied de page.';
